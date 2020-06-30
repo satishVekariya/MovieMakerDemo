9f1789face5a6e625bdd1b396a7548906d3d4f5f
satish <satish.v@canopas.com>
2020-06-30 17:37:39 +0530

    Initial Commit

---
+++ MovieMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMaker
+//
+//  Created by Satish's iMac on 30/06/20.
+//  Copyright © 2020 Canopas Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
